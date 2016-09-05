9e1040d491baab3b64c01a7c8b83a5f34b796004
Caio Souza <caiophilipevs@gmail.com>
2016-09-05 17:54:35 -0300

    Compilation directive to force PT/BR in language

--- l10n/Message.hx
+++ l10n/Message.hx
@@ -5,7 +5,16 @@ class Message
 {
 
 // Language used by all messages
-public static var language: Language;
+@:isVar public static var language(get, set): Language;
+public static function get_language()
+{
+	#if (debug && bogen_force_pt)
+		return Language.PT;
+	#else
+		return language;
+	#end
+}
+public static function set_language(value: Language) return language = value;
 
 // Messages. English is default
 private var en: String;
