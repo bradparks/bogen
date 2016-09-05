8d9b630c5b5064e1d6a8be5cb353adbaa4141477
Caio Souza <caiophilipevs@gmail.com>
2016-09-04 23:17:46 -0300

    Main activity function. Conversion from dips to pixels.

--- utils/Device.hx
+++ utils/Device.hx
@@ -1,9 +1,19 @@
 package bogen.utils;
 import bogen.l10n.Language;
+import kha.FastFloat;
+
+#if sys_android
+import com.ktxsoftware.kha.KhaActivity;
+#end
 
 // Information about the device
 class Device
 {
+
+// Current activity
+#if sys_android
+public static inline function activity() return KhaActivity.the();
+#end
 
 // Device's language
 public static inline function language(): Language
@@ -22,6 +32,24 @@ public static inline function language(): Language
 	
 	if (language == "pt") return Language.PT;
 	return Language.EN;
+}
+
+// Device's pixel independent conversion
+public static function dipToPixels(dip: Int): FastFloat
+{
+	#if sys_android
+		return untyped __java__
+		("
+			android.util.TypedValue.applyDimension
+			(
+				android.util.TypedValue.COMPLEX_UNIT_DIP,
+				dip,
+				activity().getResources().getDisplayMetrics()
+			)
+		");
+	#else
+		return dip;
+	#end
 }
 
 }
