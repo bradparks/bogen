d23f9aeda77e7c6c11e481e88a2ff24668d84a9c
Caio Souza <caiophilipevs@gmail.com>
2016-09-08 15:12:23 -0300

    Frame step with ctrl key

--- simulation/Scene.hx
+++ simulation/Scene.hx
@@ -12,31 +12,47 @@ class Scene extends Simulation
 // Pauses the scene
 private var pause: Bool;
 
+// Should the game step a frame?
+private var updateStep: Bool;
+private var renderStep: Bool;
+
 // Constructor
 public function new()
 {
 	super();
+	
 	pause = false;
+	
+	updateStep = false;
+	renderStep = false;
 }
 
 // Input
 override public function onInput(input: Input) 
 {
+	
 	if (input.keyType == Key.ENTER) pause = !pause;
+	else if (input.keyType == Key.CTRL)
+	{
+		updateStep = renderStep = true;
+		pause = true;
+	}
 	super.onInput(input);
 }
 
 // Update
 override public function onUpdate(timeStep: TimeStep)
 {
-	if (pause) timeStep = new TimeStep(0, 0);
+	if (pause && !updateStep) timeStep = new TimeStep(0, 0);
+	updateStep = false;
 	super.onUpdate(timeStep);
 }
 
 // Draw
 override public function onDraw(canvas: Canvas, timeStep: TimeStep)
 {
-	if (pause) timeStep = new TimeStep(0, 0);
+	if (pause && !renderStep) timeStep = new TimeStep(0, 0);
+	renderStep = false;
 	super.onDraw(canvas, timeStep);
 }
 
